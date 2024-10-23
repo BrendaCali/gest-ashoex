221e1f5e29ced91ac8fe304329ff1455fce6ca61
crexpo <crespomauricio16@gmail.com>
2024-10-23 18:20:22 -0400

    swagger funcionando

--- gest-ashoex-backend/app/Http/Controllers/GrupoController.php
+++ gest-ashoex-backend/app/Http/Controllers/GrupoController.php
@@ -7,7 +7,18 @@ use App\Models\Grupo;
 
 class GrupoController extends Controller
 {
-
+/**
+ * @OA\Get(
+ *     path="/api/grupo",
+ *     tags={"Grupos"},
+ *     summary="Obtener lista de grupos",
+ *     description="Este endpoint retorna una lista de grupos.",
+ *     @OA\Response(
+ *         response=200,
+ *         description="Lista de grupos obtenida con éxito",
+ *     )
+ * )
+ */
     public function index()
     {
         try{    
@@ -72,9 +83,59 @@ class GrupoController extends Controller
         ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
+ /**
+ * @OA\Get(
+ *     path="/api/grupo/{id}",
+ *     tags={"Grupos"},
+ *     summary="Obtiene los detalles de una grupo por su ID",
+ *     @OA\Parameter(
+ *         name="id",
+ *         in="path",
+ *         required=true,
+ *         @OA\Schema(type="integer"),
+ *         description="ID de la grupo"
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="Operación exitosa",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(property="success", type="boolean", example=true),
+ *             @OA\Property(property="data", type="object",
+ *                 @OA\Property(property="id", type="integer", example=1),
+ *                 @OA\Property(property="materia_id", type="integer", example="1"),
+ *                 @OA\Property(property="nro_grupo", type="number", example=1)
+ *             ),
+ *             @OA\Property(property="error", type="array", @OA\Items(type="string"), example={}),
+ *             @OA\Property(property="message", type="string", example="Operación exitosa")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=404,
+ *         description="El grupo  no existe",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(property="success", type="boolean", example=false),
+ *             @OA\Property(property="data", type="array", @OA\Items(), example={}),
+ *             @OA\Property(property="error", type="array", 
+ *                 @OA\Items(type="string", example="El grupo no existe")),
+ *             @OA\Property(property="message", type="string", example="Operación fallida")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=500,
+ *         description="Error en el servidor interno",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             @OA\Property(property="success", type="boolean", example=false),
+ *             @OA\Property(property="data", type="array", @OA\Items(), example={}),
+ *             @OA\Property(property="error", type="array", 
+ *                 @OA\Items(type="string", example="Ocurrió un error inesperado")),
+ *             @OA\Property(property="message", type="string", example="Operación fallida")
+ *         )
+ *     )
+ * )
+ */
     public function show(string $id)
     {
         $grupo = Grupo::find($id);
